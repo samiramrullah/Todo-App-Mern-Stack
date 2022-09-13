155f4eca758636681a4248a0c542632a47ff7276
samiramrullah <samiramrullah@gmail.com>
2022-09-13 15:57:59 +0530

    with routes

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,12 @@
 const express=require('express');
 const app=express();
+const morgan=require('morgan');
+const bodyParser=require('body-parser');
 const task=require('./routes/task');
 
 
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+app.use(morgan('dev'))
 app.use('/task',task);
 module.exports=app;
--- backend/routes/task.js
+++ backend/routes/task.js
@@ -1,26 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-
-// router.get('/createtask', (req, res, next) => {
-//     res.status(201).json({
-//         message: "task added",
-//     })
-// });
-
-// router.post('/deleteall', (req, res, next) => {
-//     res.status(202).json({
-//         message: "Delete api",
-//     })
-// });
-
-// router.get("/posts/:id",(req,res,next)=>{
-//     const id=req.params.id;
-//     console.log(id);
-//     res.status(200).json({
-//         id:id,
-//     })
-// })
+router.post('/createtask',(req,res,next)=>{
+    const task=req.body.task;
+    res.status(201).json({
+        message:"Task Created",
+    })
+});
 router.get('/deletebyId/:id',(req,res,next)=>{
       const id=req.params.id;
       res.status(200).json({
@@ -28,11 +14,11 @@ router.get('/deletebyId/:id',(req,res,next)=>{
         id:id,
       })
 });
-// router.post('/editbyId/:id',(req,res,next)=>{
-//     const id=usePrams(id);
-//     res.status(204).json({
-//         message:'edited',
-//         id:id,
-//     })
-// })
+router.patch('/updatebyId/:id',(req,res,next)=>{
+    const id=req.params.id;
+    res.status(200).json({
+        message:"updated",
+        id:id
+    })
+})
 module.exports = router;

72b1dc610ccbb948391e68f4c1ccf0de978ece78
samiramrullah <samiramrullah@gmail.com>
2022-09-13 17:03:34 +0530

    with modal

--- frontend/src/Components/Task.js
+++ frontend/src/Components/Task.js
@@ -1,13 +1,19 @@
 import { Card } from 'antd';
+import { useState } from 'react';
+import BasicModal from '../modal/BasicModal';
 const Task = () => {
     const gridStyle = {
         width: '100%',
-        // textAlign: 'center',
     };
+    const closeModal=()=>{
+        setisModalOpen(false)
+    }
+    const [isModalOpen,setisModalOpen]=useState(false);
     return (
-        <div>
-            <Card title="Card Title">
-                <Card.Grid style={gridStyle}>Buy Laptop</Card.Grid>
+        <div >
+            <Card title="Remaining Task" >
+                <Card.Grid onClick={()=>setisModalOpen(true)} style={gridStyle}>Buy Laptop</Card.Grid >
+                {isModalOpen && <BasicModal isModalOpen={isModalOpen} setIsModalOpen={closeModal}/>}
             </Card>
         </div>
     )
---
+++ frontend/src/modal/BasicModal.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Modal } from 'antd';
+const BasicModal = ({ isModalOpen, setIsModalOpen }) => {
+    const handleOk = () => {
+        setIsModalOpen(false);
+    };
+
+    const handleCancel = () => {
+        setIsModalOpen(false);
+    };
+    return (
+        <>
+            <Modal title="Basic Modal" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+                <p>Some contents...</p>
+                <p>Some contents...</p>
+                <p>Some contents...</p>
+            </Modal>
+        </>
+    )
+}
+
+export default BasicModal

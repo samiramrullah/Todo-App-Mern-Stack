d3d93edfd663c94e65f3a7d80f8b310d07505fd2
samiramrullah <samiramrullah@gmail.com>
2022-09-13 18:53:19 +0530

    with api

--- frontend/src/Components/Task.js
+++ frontend/src/Components/Task.js
@@ -1,31 +1,46 @@
+import React from 'react';
 import { Button, Card } from 'antd';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios'
 const Task = () => {
+    useEffect(() => {
+        axios.get('http://localhost:5000/task/getalltask')
+            .then(res => setalltasks(res.data))
+            .catch(err => console.log(err))
+    }, []);
     const gridStyle = {
         width: '100%',
     };
     const [showedit, setshowedit] = useState(false);
-    const editButtonHandler=()=>{
-         if(showedit===false)
-         {
+    const [alltasks, setalltasks] = useState();
+    const editButtonHandler = () => {
+        if (showedit === false) {
             setshowedit(true);
-         }
-         else
-         {
+        }
+        else {
             setshowedit(false);
-         }
+        }
     }
-    const deleteHandler=()=>{
+    const deleteHandler = () => {
         window.alert('deleted')
     }
+    console.log(alltasks);
     return (
         <div >
             <Card title="Remaining Task" >
                 <Card.Grid style={gridStyle}>{showedit === true ? (
                     <input />
                 ) : (
-                    <p>Buy laptop</p>
-                )} <Button onClick={deleteHandler}>Delete</Button> <Button onClick={editButtonHandler}>{showedit===true?(<p>Save</p>):(<p>Edit</p>)}</Button>  </Card.Grid >
+                    <div>
+                        {alltasks && (
+                            alltasks.tasks.map(itm => (
+                                <>
+                                    <p>{itm.task}</p>
+                                    <Button onClick={deleteHandler}>Delete</Button> <Button onClick={editButtonHandler}>{showedit === true ? (<p>Save</p>) : (<p>Edit</p>)}</Button>  </>
+                            ))
+                        )}
+                    </div>
+                )} </Card.Grid >
             </Card>
         </div>
     )
